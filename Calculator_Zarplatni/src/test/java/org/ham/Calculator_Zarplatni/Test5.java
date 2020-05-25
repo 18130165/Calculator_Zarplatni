76b8ebfd495e67f127cd5a1e9600dcbedf32c667
18130165 <emil27.03@inbox.ru>
2020-05-25 16:35:47 +0500

    Изменен

--- Calculator_Zarplatni/src/test/java/org/ham/Calculator_Zarplatni/Test5.java
+++ Calculator_Zarplatni/src/test/java/org/ham/Calculator_Zarplatni/Test5.java
@@ -9,7 +9,7 @@ public class Test5 {
 	public void test5(){
 		ButtonEventListener prw = new ButtonEventListener();
 		CalculGUI RTV = new CalculGUI(); //создание объекта класса CalculGUI
-		CalculGUI.input1.setText("14"); //задаем значение первого поля 
+		CalculGUI.input1.setText("7"); //задаем значение первого поля 
 		CalculGUI.input2.setText("15000"); //задаем значение второго поля 
 		CalculGUI.input3.setText("5000"); //задаем значение третьего поля 
 		CalculGUI.input4.setText("7"); //задаем значение четвертого поля 
